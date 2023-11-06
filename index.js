1b133e66ab26eec1912249f00b2d65674e177135
Spencer Toliver <tolivercapital@gmail.com>
2023-11-06 11:34:02 -0600

    Need to revise but completed block15.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//create an app that allows the user to select his yogurt preferences
+//the user needs to be able to select different flavors
+
+//create an array of chocolate, strawberry, vanilla, and melon.
+const flavors = ([`chocolate`, `strawberry`, `vanilla`, `melon`]) => {
+//the response needs to be able to seperate the different flavors
+  const userFlavor = flavors.split(` `);
+  return userFlavor([]);
+}
+
+//the user needs to be able to decide how many desired
+userInput = prompt(`Order your Flavor!`);
+
